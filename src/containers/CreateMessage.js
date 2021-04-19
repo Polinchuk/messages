e59461c251e3556dd31d17e2cdd40151bc15ca89
Polina <polina.pargina@yandex.ru>
2021-04-19 19:25:04 +0300

    190421

--- src/containers/CreateMessage.js
+++ src/containers/CreateMessage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import { addNewMessage } from '../action/';
+import url from '../components/url.json';
 
 export default class CreateMessage extends React.Component {
 
@@ -15,7 +16,9 @@ export default class CreateMessage extends React.Component {
             itemsGroups: [],
             itemsType: [],
             date: date,
-            message: props.message
+            message: props.message,
+            valueGroup: '',
+            valueType: ''
         }; 
         
         this.formHandler = this.formHandler.bind(this)
@@ -37,17 +40,41 @@ export default class CreateMessage extends React.Component {
         this.props.history.push('/');
 
     }
+
+    groupHandler = (event) => {
+        event.preventDefault();
+        let groupId = event.target.id;
+        this.state.itemsType.map(item => {
+            if (item.id === groupId) {
+                this.setState({
+                    valueType: item.name
+                });
+            }
+        });
+    }
+
+    typeHandler = (event) => {
+        event.preventDefault();
+        let typeId = event.target.id;
+        this.state.itemsGroups.map(item => {
+            if (item.id === typeId) {
+                this.setState({
+                    valueGroup: item.name
+                });
+            }
+        });
+    }
     
     componentDidMount() {
 
-        fetch("(https://pos.gosuslugi.ru/filters",{
+        fetch(url.urlRegion,{
              mode: 'no-cors'
             })
         
-            //.then(response => JSON.parse(response))
+            .then(response => response.json())
           
 
-        fetch("https://pos.gosuslugi.ru/inbox-service/subjects")
+        fetch(url.urlGroup)
             .then(response => response.json())
             .then(
                 (result) => {
@@ -64,7 +91,7 @@ export default class CreateMessage extends React.Component {
                 }
             )
 
-            fetch("https://pos.gosuslugi.ru/inbox-service/subsubjects/subject/116/region/22")
+        fetch(url.urlType)
             .then(response => response.json())
             .then(
                 (result) => {
@@ -86,7 +113,7 @@ export default class CreateMessage extends React.Component {
 
     render() {
 
-        const {error, isLoaded, itemsGroups, itemsRegion, itemsType} = this.state;
+        const {error, isLoaded, itemsGroups, itemsRegion, itemsType, valueGroup, valueType} = this.state;
        if (error) {
             return <p>Error {error.message}</p>
         } else if (!isLoaded) {
@@ -104,25 +131,25 @@ export default class CreateMessage extends React.Component {
                             <select className="custom-select" id="formRegion" defaultValue="">
                                 <option disabled selected hidden>Выберите регион</option>
                                 {itemsRegion.map(item => (
-                                    <option key={item.id} required value={item.name}>
+                                    <option key={item.id} value={item.name} required>
                                         {item.name}
                                     </option>
                                 ))}
                             </select>
                             <label htmlFor="formGroup">Выберите группу</label>
-                            <select className="custom-select" id="formGroup" defaultValue="Выбрать из списка" required>
-                                <option disabled selected hidden>Выбрать из списка</option>
+                            <select className="custom-select" id="formGroup" required>
+                                {valueGroup ? (<option disabled selected hidden>{valueGroup}</option>) : (<option disabled selected hidden>Выбрать из списка</option>)}
                                 {itemsGroups.map(item => (
-                                    <option key={item.id} value={item.name}>
+                                    <option key={item.id} value={item.name} id={item.id} onClick={this.groupHandler}>
                                         {item.name}
                                     </option>
                                 ))}
                             </select>
                             <label htmlFor="formType">Выберите тип обращения</label>
-                            <select className="custom-select" id="formType" defaultValue="" required>
-                                <option disabled selected hidden>Выбрать из списка</option>
+                            <select className="custom-select" id="formType" required>
+                                {valueType ? (<option disabled selected hidden>{valueType}</option>) : (<option disabled selected hidden>Выбрать из списка</option>)}
                                 {itemsType.map(item => (
-                                    <option key={item.id} value={item.name}>
+                                    <option key={item.id} value={item.name} id={item.subject.id} onClick={this.typeHandler}>
                                         {item.name}
                                     </option>
                                 ))}
